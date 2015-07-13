fee08f6b99459e3eb1dcaada7119e8085694fac9
Henry Firth <henrynf12@gmail.com>
2015-07-12 20:18:49 -0500

    Add pipes

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var mainState = {
     game.stage.backgroundColor = '#44D3A8';
 
     game.load.image('bird', 'assets/bird.png');
+    game.load.image('pipe', 'assets/pipe.png');
 
   },
 
@@ -22,6 +23,12 @@ var mainState = {
     var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     spaceKey.onDown.add(this.jump, this);
 
+    this.pipes = game.add.group();
+    this.pipes.enableBody = true;
+    this.pipes.createMultiple(20, 'pipe');
+
+    this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+
   },
 
   update: function() {
@@ -35,6 +42,31 @@ var mainState = {
   jump: function() {
 
     this.bird.body.velocity.y = -350;
+
+  },
+
+  addOnePipe: function(x,y) {
+
+    var pipe = this.pipes.getFirstDead();
+
+    pipe.reset(x,y);
+
+    pipe.body.velocity.x = -200;
+
+    pipe.checkWorldBounds = true;
+    pipe.outOfBoundsKill = true;
+
+  },
+
+  addRowOfPipes: function() {
+
+    var hole = Math.floor(Math.random()*5)+1;
+
+    for (var i = 0; i < 8; i++) {
+      if (i != hole && i != hole + 1) {
+        this.addOnePipe(400, i*60 + 10);
+      }
+    }
 
   },
 
