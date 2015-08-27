0b8a160201911f88b57e22d3ad8938caf90eb354
Henry Firth <henrynf12@gmail.com>
2015-08-27 16:12:24 -0500

    Add fail phrases and fix score bug

--- main.js
+++ main.js
@@ -1,137 +1,149 @@
-var game = new Phaser.Game(400, 490, Phaser.AUTO, 'gameDiv');
+window.onload = function() {
 
-var mainState = {
+  var game = new Phaser.Game(400, 490, Phaser.AUTO, 'gameDiv');
 
-  preload: function() {
+  var mainState = {
 
-    game.stage.backgroundColor = '#44D3A8';
+    preload: function() {
 
-    game.load.image('bord', 'assets/bord.png');
-    game.load.image('machine', 'assets/machine.png');
+      game.stage.backgroundColor = '#00D1D3';
 
-    game.load.audio('jump', 'assets/jump.wav');
+      game.load.image('bird', 'assets/bird.png');
+      game.load.image('pipe', 'assets/pipe.png');
 
-  },
+      game.load.audio('jump', 'assets/jump.wav');
 
-  create: function() {
+    },
 
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+    create: function() {
 
-    this.bord = this.game.add.sprite(100, 245, 'bord');
+      game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    game.physics.arcade.enable(this.bord);
-    this.bord.body.gravity.y = 1000;
+      this.bird = this.game.add.sprite(100, 245, 'bird');
 
-    this.bord.anchor.setTo(-0.2, 0.5);
+      this.phrases = ['WHOOPS!', 'TRY AGAIN!']
 
-    var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-    spaceKey.onDown.add(this.jump, this);
+      game.physics.arcade.enable(this.bird);
+      this.bird.body.gravity.y = 1000;
 
-    this.pipes = game.add.group();
-    this.pipes.enableBody = true;
-    this.pipes.createMultiple(20, 'machine');
+      this.bird.anchor.setTo(-0.2, 0.5);
 
-    this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+      var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+      spaceKey.onDown.add(this.jump, this);
 
-    this.score = 0;
-    this.labelScore = game.add.text(20,20,'0',{
-      font: "30px Impact",
-      fill: "#ffffff",
-      stroke: "#000000",
-      strokeThickness: 5
-    });
+      this.pipes = game.add.group();
+      this.pipes.enableBody = true;
+      this.pipes.createMultiple(20, 'pipe');
 
-    this.jumpSound = game.add.audio('jump');
+      this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+      this.numberPipes = 0;
 
-  },
+      this.score = 0;
+      this.labelScore = game.add.text(20,20,'0',{
+        font: "30px Impact",
+        fill: "#ffffff",
+        stroke: "#000000",
+        strokeThickness: 5
+      });
 
-  update: function() {
+      this.jumpSound = game.add.audio('jump');
 
-    if (this.bord.inWorld == false) {
-      this.restartGame();
-    }
+    },
 
-    if (this.bord.angle < 20) {
-      this.bord.angle += 1;
-    }
-    game.physics.arcade.overlap(this.bord, this.pipes, this.hitPipe, null, this);
+    update: function() {
 
-  },
-
-  jump: function() {
-    if (this.bord.alive == false) {
-      return;
-    }
-
-    game.add.tween(this.bord).to({angle: -20}, 100).start();
-
-    this.bord.body.velocity.y = -350;
-
-    this.jumpSound.play();
-
-  },
-
-  addOnePipe: function(x,y) {
-
-    var machine = this.pipes.getFirstDead();
-
-    machine.reset(x,y);
-
-    machine.body.velocity.x = -200;
-
-    machine.checkWorldBounds = true;
-    machine.outOfBoundsKill = true;
-
-  },
-
-  addRowOfPipes: function() {
-
-    var hole = Math.floor(Math.random()*5)+1;
-
-    for (var i = 0; i < 8; i++) {
-      if (i != hole && i != hole + 1) {
-        this.addOnePipe(400, i*60 + 10);
+      if (this.bird.inWorld == false) {
+        this.restartGame();
       }
+
+      if (this.bird.angle < 20) {
+        this.bird.angle += 1;
+      }
+      game.physics.arcade.overlap(this.bird, this.pipes, this.hitPipe, null, this);
+
+    },
+
+    jump: function() {
+      if (this.bird.alive == false) {
+        return;
+      }
+
+      game.add.tween(this.bird).to({angle: -20}, 100).start();
+
+      this.bird.body.velocity.y = -350;
+
+      this.jumpSound.play();
+
+    },
+
+    randomPhrase: function() {
+      return this.phrases[Math.floor(Math.random()*this.phrases.length)];
+    },
+
+    addOnePipe: function(x,y) {
+
+      var pipe = this.pipes.getFirstDead();
+
+      pipe.reset(x,y);
+
+      pipe.body.velocity.x = -200;
+
+      pipe.checkWorldBounds = true;
+      pipe.outOfBoundsKill = true;
+
+    },
+
+    addRowOfPipes: function() {
+
+      var hole = Math.floor(Math.random()*5)+1;
+
+      for (var i = 0; i < 8; i++) {
+        if (i != hole && i != hole + 1) {
+          this.addOnePipe(400, i*60 + 10);
+        }
+      }
+
+      this.numberPipes += 1;
+      this.score = this.numberPipes - 1;
+      this.labelScore.text = this.score;
+
+    },
+
+    hitPipe: function() {
+
+      if (this.bird.alive == false) {
+        return;
+      }
+
+      this.bird.alive = false;
+
+      game.time.events.remove(this.timer);
+
+      this.pipes.forEachAlive(function(p) {
+        p.body.velocity.x = 0;
+      });
+
+      this.failMsg = game.add.text(this.bird.x+60, this.bird.y-20, this.randomPhrase(), {
+        font: "30px Impact",
+        fill: "#ffffff",
+        stroke: "#000000",
+        strokeThickness: 5
+      });
+
+      this.add.tween(this.failMsg).to({y: this.bird.y-50}, 500, Phaser.Easing.Linear.None, true);
+      game.add.tween(this.failMsg).to({alpha: 0}, 500, Phaser.Easing.Linear.None, true);
+
+    },
+
+    restartGame: function() {
+
+      game.state.start('main');
+
     }
 
-    this.score += 1;
-    this.labelScore.text = this.score;
+  };
 
-  },
-
-  hitPipe: function() {
-
-    if (this.bord.alive == false) {
-      return;
-    }
-
-    this.bord.alive = false;
-
-    game.time.events.remove(this.timer);
-
-    this.pipes.forEachAlive(function(p) {
-      p.body.velocity.x = 0;
-    });
-
-    this.failMsg = game.add.text(this.bord.x+60, this.bord.y-20, 'SHIT', {
-      font: "30px Impact",
-      fill: "#ffffff",
-      stroke: "#000000",
-      strokeThickness: 5
-    });
-
-    this.add.tween(this.failMsg).to({y: this.bord.y-50}, 500, Phaser.Easing.Linear.None, true);
-    game.add.tween(this.failMsg).to({alpha: 0}, 500, Phaser.Easing.Linear.None, true);
-
-  },
-
-  restartGame: function() {
-
-    game.state.start('main');
-
-  }
+  game.state.add('main', mainState);
+  game.state.start('main');
 
 };
-
-game.state.add('main', mainState);
-game.state.start('main');
